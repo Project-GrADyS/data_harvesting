c555860f2f6bd540a096afc70f54f281ca3fca8f
Thlamz <thlamenza@hotmail.com>
2025-09-25 12:10:17 -0300

    Setting up longer batch run

--- main.py
+++ main.py
@@ -14,37 +14,3 @@ if __name__ == "__main__":
         config: dict = yaml.safe_load(f)
     
     mlflow.set_experiment(args.experiment_id)
-
-    space = {
-        "optimization": {
-            "num_optimizer_steps": hp.choice("num_optimizer_steps", [1, 5, 10]),
-            "lr": hp.loguniform("lr", -10, -3),
-            "grad_clip": hp.choice("grad_clip", [0, 0.5, 1.0, 5.0]),
-        }
-    }
-
-    def tune(tune_args: dict):
-        run_config = config.copy()
-        
-        # tune_args contains overrides that should be merged into the base config
-        for section, params in tune_args.items():
-            if section not in run_config:
-                run_config[section] = {}
-            for key, value in params.items():
-                run_config[section][key] = value
-
-        lr = run_config['optimization']['lr']
-        num_optimizer_steps = run_config['optimization']['num_optimizer_steps']
-        grad_clip = run_config['optimization']['grad_clip']
-        return -train(run_config, 
-                      run_name=f"lr_{lr}_steps_{num_optimizer_steps}_clip_{grad_clip}")
-
-    best = fmin(
-        fn=tune,
-        space=space,
-        algo=tpe.suggest,
-        max_evals=20,
-        show_progressbar=False
-    )
-    print("Best hyperparameters found:")
-    print(best)
