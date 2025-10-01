f4213a0bffa519b6e81d783a03a1d50200896100
Thlamz <thlamenza@hotmail.com>
2025-10-01 10:23:58 -0300

    Making exp name optional
    Greatly facilitates debugging.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from hyperopt import hp, fmin, tpe
 from train import train
 
 argparse = argparse.ArgumentParser()
-argparse.add_argument("-E", type=str, required=True, help="MLflow experiment ID", dest="experiment_id")
+argparse.add_argument("-E", type=str, required=False, help="MLflow experiment ID", dest="experiment_name")
 args = argparse.parse_args()
 
 mlflow.set_tracking_uri("file:./mlruns")
@@ -15,5 +15,6 @@ if __name__ == "__main__":
     with open("params.yaml", "r") as f:
         config: dict = yaml.safe_load(f)
     
-    mlflow.set_experiment(args.experiment_id)
+    if args.experiment_name:
+        mlflow.set_experiment(args.experiment_name)
     train(config)
