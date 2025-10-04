f24d50a2093057835062112ab6699456178b6734
Thlamz <thlamenza@hotmail.com>
2025-10-04 10:59:16 -0300

    Setting up longer run for best hyperparameters yet

--- main.py
+++ main.py
@@ -18,38 +18,4 @@ if __name__ == "__main__":
     if args.experiment_name:
         mlflow.set_experiment(args.experiment_name)
 
-    space = {
-        "optimization": {
-            "num_optimizer_steps": hp.choice("num_optimizer_steps", [1, 5, 10]),
-            "lr": hp.loguniform("lr", -10, -3),
-            "grad_clip": hp.choice("grad_clip", [0, 0.5, 1.0, 5.0]),
-        }
-    }
-
-    def tune(tune_args: dict):
-        run_config = config.copy()
-        
-        tuned_arg_leafs: tuple[str, any] = []
-
-        # tune_args contains overrides that should be merged into the base config
-        for section, params in tune_args.items():
-            if section not in run_config:
-                run_config[section] = {}
-            for key, value in params.items():
-                run_config[section][key] = value
-                tuned_arg_leafs.append((f"{section}.{key}", value))
-
-        run_name = " ".join([f"{k}={v}" for k, v in tuned_arg_leafs])
-
-        return -train(run_config, 
-                      run_name=run_name)
-
-    best = fmin(
-        fn=tune,
-        space=space,
-        algo=tpe.suggest,
-        max_evals=30,
-        show_progressbar=False
-    )
-    print("Best hyperparameters found:")
-    print(best)
+    train(config)
